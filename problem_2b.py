90025c34d30857842910d0878f43c5e4f5b1e849
Brian To <bht170030@utdallas.edu>
2019-09-05 16:49:44 -0500

    added linear seperating plane to the plot

--- problem_2b.py
+++ problem_2b.py
@@ -4,7 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
+
+xs = np.linspace(0.9, 1.07, 10)
+zs = np.linspace(-2.0, 1.0, 10)
+X, Z = np.meshgrid(xs, zs)
+Y = X - 1
+
+
+ax.plot_surface(X, Z, Y, color=(0,1,0,0.2),shade=False)
 
 
 zdata = 1
@@ -13,6 +21,11 @@ ydata = 1
 
 ax.scatter(xdata,ydata,zdata)
 ax.scatter(xdata,ydata,-zdata,c='Red')
+
+
+plt.ylim((.95, 1.05))
+plt.xlim((.95, 1.05))
+ax.set_zlim(-2,1.0)
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
