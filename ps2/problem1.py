b280ae31badd6c7b60aed934c82b804a6a629bca
BrianT <bht170030@utdallas.edu>
2019-09-22 13:43:07 -0500

    init ps2

---
+++ ps2/problem1.py
@@ -0,0 +1,63 @@
+import numpy as np
+from cvxopt import solvers, matrix
+import math
+
+
+def read_data(filename):
+    data = []
+    with open(filename) as file:
+        for line in file:
+            data_line = [float(item) for item in line.split(',')]
+            data.append(data_line)
+    data = np.array(data)
+    labels = data[:, -1] # grab all the labels
+    data = data[:, 0:4] # grab all the vectors
+
+    # print(type(labels),type(data))
+    return (data, labels)
+
+def feature_mapping(old_data):
+    mapped_data = []
+    for old_row in old_data:
+        new_row = list(math.pow(old_item, 3.0) for old_item in old_row[0:4]) +  list((math.pow(old_item, 2.0) * np.exp(old_item)) for old_item in old_row[0:4])#+ list(math.pow(old_item, 2.0) for old_item in old_row[0:4]) #+ list(math.pow(old_item, 5.0) for old_item in old_row[1:2]) + list(math.pow(old_item, 3.0) for old_item in old_row[2:4]) + [12]
+        mapped_data.append(new_row)
+
+    return (np.array(mapped_data))
+
+
+raw_data = read_data("mystery.data")
+data = raw_data[0]
+labels = raw_data[1]
+
+data = feature_mapping(data)
+dim = len(data[0]) + 1
+
+P = np.eye(dim)
+P[-1,-1] = 0.0
+P = 2 * P
+
+P = matrix(P)
+q = matrix(np.zeros(dim))
+h = matrix(-1 * np.ones((len(data),1)))
+G = np.zeros((len(data), dim))
+
+for row in range(len(data)):
+    G[row] = np.hstack((-1 * labels[row] * data[row], -1 * labels[row]))
+
+G = matrix(G)
+
+sol = solvers.qp(P, q, G, h)
+# print(sol['x'])
+
+sol_arr = np.array(sol['x'])
+w = sol_arr[:dim-1]
+b = sol_arr[-1]
+
+print("\nw: " + str(w) + '\n')
+print("b: " + str(b))
+
+supvectpos = []
+supvectneg = []
+for i in range(len(data)):
+    # passb = y[S] - np.dot(X[S], w)
+    pass
