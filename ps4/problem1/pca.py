240615fd62e2ae4ea756fddc65f310663f1cffab
BrianT <bht170030@utdallas.edu>
2019-11-04 03:10:29 -0600

    started ps4

--- ps4/problem1/pca.py
+++ ps4/problem1/pca.py
@@ -2,5 +2,28 @@ import numpy as np
 import csv
 
 
-data = np.loadtxt(fname="sonar_train.data", delimiter=',')
+all_data = np.loadtxt(fname="sonar_train.data", delimiter=',')
 
+
+
+labels = all_data[:,-1]
+data = all_data[: , 0:-1]
+
+
+
+def pca(data):
+    from sklearn import preprocessing
+    # mus = []
+    # data = data.T
+    # for i in data:
+    #     mus.append(sum(i) / len(i))
+
+    # data = data.T
+    # for i in range(len(data)):
+    #     data[i] = np.subtract(data[i], mus)
+    
+    scaled_data = preprocessing.scale(data)
+    
+        
+
+pca(data)
