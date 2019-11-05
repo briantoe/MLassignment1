08c345f9a4a73c895594dc306174ede7e895c403
BrianT <bht170030@utdallas.edu>
2019-11-05 12:17:52 -0600

    Formulating SVM Thingy

--- ps4/problem1/pca.py
+++ ps4/problem1/pca.py
@@ -1,29 +1,51 @@
 import numpy as np
 import csv
-
-
-all_data = np.loadtxt(fname="sonar_train.data", delimiter=',')
-
-
-
-labels = all_data[:,-1]
-data = all_data[: , 0:-1]
-
+import copy
 
 
 def pca(data):
-    from sklearn import preprocessing
+    M = np.mean(data.T, axis=1)
+    C = data - M
+    WWT = np.cov(C.T)
+    
+    eig_vals, eig_vect = np.linalg.eig(WWT)
+    print(sorted(eig_vals, reverse=True))
+
+    return sorted(eig_vals, reverse=True)
+    # from sklearn.preprocessing import scale, normalize
+    # scaled_data = preprocessing.scale(data)
+    # print(scaled_data)
     # mus = []
+    # p = len(data)
     # data = data.T
-    # for i in data:
-    #     mus.append(sum(i) / len(i))
+    # for i in data: 
+    #     mus.append(sum(i) / float(p))
 
     # data = data.T
-    # for i in range(len(data)):
-    #     data[i] = np.subtract(data[i], mus)
-    
-    scaled_data = preprocessing.scale(data)
-    
-        
+    # w = copy.deepcopy(data)
+    # for i in range(len(w)):
+    #     w[i] = np.subtract(w[i], mus)
 
-pca(data)
+    
+    # wwt = np.matmul(w, w.T)
+    # eig_val, eig_vect = np.linalg.eig(wwt)
+    # print(sorted(eig_val.real,reverse=True))
+    # print(eig_vect)
+    # wwt = np.cov(w)
+    # eigs, normeigs = np.linalg.eig(wwt)
+    # print(eigs)
+    # print(normeigs)
+
+def svm(data, eig_vals):
+    
+
+def main():
+    all_data = np.loadtxt(fname="sonar_train.data", delimiter=',')
+    labels = all_data[:,-1]
+    data = all_data[: , 0:-1]
+
+    eig_vals = pca(data)[0:6]
+
+
+if __name__ == "__main__":
+    main()
