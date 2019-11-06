d46b0018669dc12833ba3882056242a77bc75d67
Brian To <bht170030@utdallas.edu>
2019-11-05 21:39:01 -0600

    Checkpoint before svm

--- ps4/problem1/pca.py
+++ ps4/problem1/pca.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 import copy
+np.set_printoptions(threshold=1000)
 
 
 def pca(data):
@@ -9,42 +10,28 @@ def pca(data):
     WWT = np.cov(C.T)
     
     eig_vals, eig_vect = np.linalg.eig(WWT)
-    print(sorted(eig_vals, reverse=True))
-
-    return sorted(eig_vals, reverse=True)
-    # from sklearn.preprocessing import scale, normalize
-    # scaled_data = preprocessing.scale(data)
-    # print(scaled_data)
-    # mus = []
-    # p = len(data)
-    # data = data.T
-    # for i in data: 
-    #     mus.append(sum(i) / float(p))
-
-    # data = data.T
-    # w = copy.deepcopy(data)
-    # for i in range(len(w)):
-    #     w[i] = np.subtract(w[i], mus)
-
+    # print(eig_vals, eig_vect)
+    # print(sorted(eig_vals, reverse=True))
     
-    # wwt = np.matmul(w, w.T)
-    # eig_val, eig_vect = np.linalg.eig(wwt)
-    # print(sorted(eig_val.real,reverse=True))
-    # print(eig_vect)
-    # wwt = np.cov(w)
-    # eigs, normeigs = np.linalg.eig(wwt)
-    # print(eigs)
-    # print(normeigs)
 
-def svm(data, eig_vals):
+    return (sorted(eig_vals, reverse=True), [x for _,x in sorted(zip(eig_vals,eig_vect), reverse=True)])
+   
+
+# def svm(data, eig_vals):
     
 
 def main():
+    np.set_printoptions(threshold=1000)
     all_data = np.loadtxt(fname="sonar_train.data", delimiter=',')
     labels = all_data[:,-1]
     data = all_data[: , 0:-1]
 
-    eig_vals = pca(data)[0:6]
+    eig_vals, eig_vects = pca(data)
+ 
+    # only care for first 6 eigenvalues 
+    print("First 6 eigenvalues of the data covariance matrix: ")
+    print(eig_vals[0:6])
+    
 
 
 if __name__ == "__main__":
