02feebd04c9bbd36826584b024e94b387078556b
Brian To <bht170030@utdallas.edu>
2019-11-06 16:34:43 -0600

    finished 1.1 and 1.2

--- ps4/problem1/pca.py
+++ ps4/problem1/pca.py
@@ -1,7 +1,6 @@
 import numpy as np
 import csv
 import copy
-import svmwithslack
 np.set_printoptions(threshold=1000)
 
 
@@ -33,7 +32,6 @@ def main():
     print("First 6 eigenvalues of the data covariance matrix: ")
     print(eig_vals[0:6])
 
-    svmwithslack.train_classifier(data, labels, np.array(eig_vals), np.array(eig_vects))
     
 
 
